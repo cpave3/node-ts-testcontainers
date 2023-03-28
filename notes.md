9ce8dc1b0331a1c7b006c22a554721cfb8e32018
Cameron Pavey <cpave3@gmail.com>
2023-03-29 01:21:23 +1100

    update code

--- notes.md
+++
@@ -1,5 +0,0 @@
-1. npm i --save-dev typescript @types/jest ts-jest jest
-2. npx tsc --init
-3. tsconfig
-4. test script
-5. jest config
---
+++ sequelize.test.ts
@@ -0,0 +1,101 @@
+import { MySqlContainer, PostgreSqlContainer } from "testcontainers";
+import { getSequelize } from "./sequelize";
+
+describe("should connect and return a query result", () => {
+  it("works with postgres", async () => {
+    // Create a container using testcontainers
+    const container = await new PostgreSqlContainer().start();
+
+    // Use the details from the container, create the database client
+    const { client, User } = await getSequelize({
+      database: container.getDatabase(),
+      username: container.getUsername(),
+      password: container.getPassword(),
+      host: container.getHost(),
+      port: container.getPort(),
+      dialect: "postgres",
+    });
+
+    // Check if you have access to the database
+    await client.authenticate();
+
+    // Create a user
+    await User.create({
+      firstName: "John",
+      lastName: "Hancock",
+    });
+
+    // Get all users
+    const users = await User.findAll();
+
+    // Check that the expected user is present
+    expect(users).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          firstName: "John",
+          lastName: "Hancock",
+          id: 1,
+          createdAt: expect.any(Date),
+          updatedAt: expect.any(Date),
+        }),
+      ])
+    );
+
+    // Close connections
+    await client.close();
+    await container.stop();
+  });
+
+  it("works with mysql", async () => {
+    // Create a container using testcontainers
+    const container = await new MySqlContainer().start();
+
+    // Use the details from the container, create the database client
+    const { client, User } = await getSequelize({
+      database: container.getDatabase(),
+      username: container.getUsername(),
+      password: container.getUserPassword(),
+      host: container.getHost(),
+      port: container.getPort(),
+      dialect: "mysql",
+    });
+
+    await client
+      .authenticate()
+      .then(() => {
+        console.log("Connection has been established successfully.");
+      })
+      .catch((error) => {
+        console.error("Unable to connect to the database: ", error);
+      });
+
+    // Check if you have access to the database
+    const auth = await client.authenticate();
+
+    // Create a user
+    await User.create({
+      firstName: "John",
+      lastName: "Hancock",
+    });
+
+    // Get all users
+    const users = await User.findAll();
+
+    // Check that the expected user is present
+    expect(users).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          firstName: "John",
+          lastName: "Hancock",
+          id: 1,
+          createdAt: expect.any(Date),
+          updatedAt: expect.any(Date),
+        }),
+      ])
+    );
+
+    // Close connections
+    await client.close();
+    await container.stop();
+  }, 15000);
+});
